fd9374dfe11c540e014a8aa1e4cb4860a3e9654d
Robert Tuck <robtuck@gmail.com>
2018-01-29 16:58:07 -0500

    added POST /videos route to app.js to give a 201 response to POST /videos test

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/videos', (req, rest) => {
+  rest.status(201).send();
+});
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
