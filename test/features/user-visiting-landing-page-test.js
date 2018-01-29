695306d8153081be0232175bf71cdd88987a5766
Robert Tuck <robtuck@gmail.com>
2018-01-29 16:19:14 -0500

    created test for user visiting landing page with no videos showing no videos.  Implemented code on index.html to pass test

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,18 @@
 const {assert} = require('chai');
+
+describe('User visiting landing page', () => {
+  describe('with no existing videos', () => {
+    it('shows no videos', () => {
+      browser.url('/');
+
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+
+  it('can navigate to create oage', () => {
+    browser.url('/');
+    browser.click('a[href="/videos/create.html"]');
+
+    assert.include(browser.getText('body'), 'Save a video');
+  });
+});
