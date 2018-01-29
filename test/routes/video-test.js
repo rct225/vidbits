2cecca362b0390330f4adae863cba70ca24356fd
Robert Tuck <robtuck@gmail.com>
2018-01-29 17:11:44 -0500

    added test in route/test/video-test.js to see if submitted video is actually saved.

--- test/routes/video-test.js
+++ test/routes/video-test.js
@@ -1,8 +1,18 @@
 const {assert} = require('chai');
 const request = require('supertest');
 const app = require('../../app');
+//const {connectDatabase, disconnectDatabase} = require('../database-utilities');
+const {mongoose, databaseUrl, options} = require('../../database');
 
 describe('POST /videos', () => {
+  beforeEach(async () => {
+    await mongoose.connect(databaseUrl, options);
+    await mongoose.connection.db.dropDatabase();
+  });
+
+  afterEach(async () => {
+     await mongoose.disconnect();
+  });
 
   it('responds with a 201 status code', async () => {
     const title = 'Cats';
@@ -14,5 +24,18 @@ describe('POST /videos', () => {
       .send({title, description})
 
     assert.equal(response.status, 201);
+  })
+
+  it('saves a Video document', async () => {
+    const title = 'Cats';
+    const description = 'Everyone like Cats';
+
+    const response = await request(app)
+      .post('/videos')
+      .type('form')
+      .send({title, description})
+
+    const video = await Video.findOne({});
+    assert.include(video, {title, description});
   })
 });
