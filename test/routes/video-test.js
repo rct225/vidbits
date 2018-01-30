f57de5b2e95f7ccbd4ef28f22cc9e0f5a8accf11
Robert Tuck <robtuck@gmail.com>
2018-01-30 09:42:16 -0500

    added test to see if GET / lists exsiting videos

--- test/routes/video-test.js
+++ test/routes/video-test.js
@@ -1,8 +1,42 @@
 const {assert} = require('chai');
 const request = require('supertest');
+const {jsdom} = require('jsdom');
 const app = require('../../app');
 const {connectDatabase, disconnectDatabase} = require('../database-utilities');
 const Video = require('../../models/video');
+
+const queryHTML = (htmlAsString, selector) => {
+  return jsdom(htmlAsString).querySelector(selector);
+};
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+  const selectedElement = queryHTML(htmlAsString, selector);
+
+  if (selectedElement !== null) {
+    return selectedElement.textContent.trim();
+  } else {
+    throw new Error(`No element with selector ${selector} found in HTML string`);
+  }
+};
+
+describe('GET /', () => {
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
+
+  it('renders existing Videos', async () => {
+    const video = await Video.create({
+      title: 'Cats',
+      description: 'Everyone like Cats',
+    });
+
+    const response = await request(app).get('/');
+
+    const bodyText = parseTextFromHTML(response.text, 'body');
+    //const iFrame = queryHTML(response.text, 'iframe');
+    //assert.equal(iFrame.src, video.url);
+    assert.include(bodyText, video.title);
+  });
+});
 
 describe('POST /videos', () => {
   beforeEach(connectDatabase);
