286eb3f1b687ac3e57f79867a725904a2cb09785
Robert Tuck <robtuck@gmail.com>
2018-01-30 09:37:56 -0500

    added test for user visiting landing page and showing existing videos in a list

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -8,6 +8,21 @@ describe('User visiting landing page', () => {
       assert.equal(browser.getText('#videos-container'), '');
     });
   });
+  describe('with an existing video', () => {
+    it('shows it in the list', () => {
+      const title = 'Cats';
+      const description = 'Everyone like Cats';
+
+      browser.url('/videos/create');
+      browser.setValue('#title-input', title);
+      browser.setValue('#description-input', description);
+      browser.click('#submit-button');
+      browser.url('/');
+
+      assert.equal(browser.getText('#videos-container'), title);
+      //assert.equal(browser.getAttribute('iframe', 'src'), url);
+    });
+  });
 
   it('can navigate to create page', () => {
     browser.url('/');
