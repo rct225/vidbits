5936d854ea676d360f048d5686259912da9282fa
Robert Tuck <robtuck@gmail.com>
2018-01-31 23:05:25 -0500

    added url to test/features/user-visiting-create-page-test.js

--- test/features/user-visiting-create-page-test.js
+++ test/features/user-visiting-create-page-test.js
@@ -5,15 +5,18 @@ describe('User visiting new videos page', () => {
     const videoToAdd = {
       title: 'Cats',
       description: 'Everyone like Cats',
+      url: `http://example.com/${Math.random()}`
     };
 
     browser.url('/videos/create');
     browser.setValue('#title-input', videoToAdd.title);
     browser.setValue('#description-input', videoToAdd.description);
+    browser.setValue('#url-input', videoToAdd.url);
     browser.click('#submit-button');
 
     const bodyText = browser.getText('body');
     assert.include(bodyText, videoToAdd.title);
     assert.include(bodyText, videoToAdd.description);
+    assert.equal(browser.getAttribute('iframe', 'src'), video.url);
   });
 });
