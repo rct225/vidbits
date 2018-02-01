31234f2e8db87f2d9cf831e521690f7a06ee718e
Robert Tuck <robtuck@gmail.com>
2018-02-01 15:19:02 -0500

    added test for User updating video does not create an additional video

--- test/features/user-updating-video-test.js
+++ test/features/user-updating-video-test.js
@@ -22,4 +22,27 @@ describe('User updating video', () => {
     const bodyText = browser.getText('body');
     assert.include(bodyText, newTitle);
   });
+  it('does not create an additional video', () => {
+    const videoToAdd = {
+      title: 'Cats',
+      description: 'Everyone like Cats',
+      url: `http://example.com/${Math.random()}`
+    };
+
+    browser.url('/videos/create');
+    browser.setValue('#title-input', videoToAdd.title);
+    browser.setValue('#description-input', videoToAdd.description);
+    browser.setValue('#url-input', videoToAdd.url);
+    browser.click('#submit-button');
+    browser.click('#edit');
+
+    const newTitle = 'Kittens';
+    browser.setValue('#title-input', newTitle);
+    browser.click('#submit-button');
+
+    browser.url('/');
+
+    const bodyText = browser.getText('body');
+    assert.notInclude(bodyText, videoToAdd.title);
+  });
 });
